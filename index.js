7805e5e945185a33ad09adce2640f427c9083885
Oliver Heward <oliverhewardbie@gmail.com>
2019-11-19 15:02:01 +0000

    work plz

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": "EAAHFzsBUAFcBAG4ls59326QdIvDCRKgCuWGgY47WuMeLeBr5mnfJJklhACbkbVoYfvTAuqXPOTzxHLvCzNsHvEZBELSNbtR8zlFe2HmAh8jyZB2kEei46usBcVyXZBQLTknY1upVL6pp3EH6r4yzeJNtRqoHOXBptbQeUD4Dyzlj2QKdImt" },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
