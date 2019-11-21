d7738f3e366d98f2e505fd4337b7efee124e1fe2
Oliver Heward <oliverhewardbie@gmail.com>
2019-11-21 16:56:44 +0000

    reverting as still nto working

--- index.js
+++ index.js
@@ -129,50 +129,48 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 function handleMessage(sender_psid, received_message) {
-  let response;
-  console.log('[handleMessage] - WAS CALLED');
-  
-  // Checks if the message contains text
-  if (received_message.text != "") {  
-    console.log('reached [recieved_message.text] block');  
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachment) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachment[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
+    let response;
+    
+    // Checks if the message contains text
+    if (received_message.text) {    
+      // Create the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      }
+    } else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Is this the right picture?",
+              "subtitle": "Tap a button to answer.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
         }
       }
-    }
-  } 
-  
-  // Send the response message
-  callSendAPI(sender_psid, response);    
-}
+    } 
+    
+    // Send the response message
+    callSendAPI(sender_psid, response);    
+  }
 
 function handlePostback(sender_psid, received_postback) {
   console.log('[handlePostback], ok');
