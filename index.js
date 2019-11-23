a4823ca0f116bab8da6082186e05b1f9d19c891f
Oliver Heward <oliverhewardbie@gmail.com>
2019-11-23 10:00:45 +0000

    missing bracket

--- index.js
+++ index.js
@@ -259,6 +259,7 @@ function callSendAPI(sender_psid, response) {
       console.error("Unable to send message:" + err);
     }
   });
+};
   // // Send the HTTP request to the Messenger Platform
   // request({
   //   "uri": "https://graph.facebook.com/v2.6/me/messages",
