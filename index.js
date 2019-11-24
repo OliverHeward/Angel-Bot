eebb258587161d48e10b8ea8993e76e37c78a301
Oliver Heward <oliverhewardbie@gmail.com>
2019-11-24 13:38:26 +0000

    isnt a payload

--- index.js
+++ index.js
@@ -179,8 +179,8 @@ function handlePostback(sender_psid, received_postback) {
       console.log('[switch case[Get Started]] - reached');
   } else if (typeof parseInt(payload) == 'number') {
     console.log('typof payload = number called');
-    console.log('typeof block', payload);
-    let age = parseInt(payload, 10);
+    console.log('typeof block', received_postback);
+    let age = Number(received_postback);
     console.log(age);
     if (isNaN(age)) {
       console.log('isNaN');
