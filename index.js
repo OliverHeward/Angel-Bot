f7e80fb4c8b57bdf5bc2986843f6dcf1f948a31a
Oliver Heward <oliverhewardbie@gmail.com>
2019-11-24 10:52:48 +0000

    hanging to /me/

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ function callSendAPI(sender_psid, response) {
   console.log('[request_body]', request_body);
 
   // const qs = 'access_token=' + encodeURIComponent("EAAHFzsBUAFcBACKIELF4PVb95ZBz2NA3e3NtZANS7ifvJw8shKBFp7kfjHap3i0ariDehmZCOtLuIK2xjZCojmVJDQXeZAq6vsCkLwYis09p7AWsmJ0wllmt1GubPZCb1fvaeZBlEbcZBMxWlLTJUcZCjgmG1cLX8WkHjHN7f701ZCN91ZCN5H0gBgZA");
-  return fetch("https://graph.facebook.com/v2.6/messages?=access_token=EAAHFzsBUAFcBACKIELF4PVb95ZBz2NA3e3NtZANS7ifvJw8shKBFp7kfjHap3i0ariDehmZCOtLuIK2xjZCojmVJDQXeZAq6vsCkLwYis09p7AWsmJ0wllmt1GubPZCb1fvaeZBlEbcZBMxWlLTJUcZCjgmG1cLX8WkHjHN7f701ZCN91ZCN5H0gBgZA", {
+  return fetch("https://graph.facebook.com/me/messages?=access_token=EAAHFzsBUAFcBACKIELF4PVb95ZBz2NA3e3NtZANS7ifvJw8shKBFp7kfjHap3i0ariDehmZCOtLuIK2xjZCojmVJDQXeZAq6vsCkLwYis09p7AWsmJ0wllmt1GubPZCb1fvaeZBlEbcZBMxWlLTJUcZCjgmG1cLX8WkHjHN7f701ZCN91ZCN5H0gBgZA", {
     method: "POST",
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify(request_body),
